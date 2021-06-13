aac0c3e159ffae4cf76dceb1338d2e04fc88a8cb
Carloscrm2 <55845286+Carloscrm2@users.noreply.github.com>
2021-06-13 10:16:16 -0500

    Add files via upload
    Ya se puede filtrar los elementos por categorías y precios (con jQuery)  , además se unió con los archivos RegalgosHombres.html y RegalosMujeres.html y ayuda.html

---
+++ Filtrando elementos/js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    //Agregando Clase active al primer enlace
+    $('.category_list .category_item[category="all"]').addClass("ct_item-active");
+
+    $(".category_item").click(function () {
+        var catProduct = $(this).attr("category");
+        console.log(catProduct);
+        //Agregando clase active al enlace secundario
+        $(".category_item").removeClass("ct_item-active");
+        $(this).addClass("ct_item-active");
+
+        //Ocultando Productos
+        $(".product-item").css("transform", "scale(0)");
+
+        function hideProduct() {
+            $(".product-item").hide();
+        }
+        setTimeout(hideProduct, 400);
+
+        //mostrando productos
+        function showProduct() {
+            $('.product-item[category="' + catProduct + '"]').css(
+                "transform",
+                "scale(1)"
+            );
+            $('.product-item[category="' + catProduct + '"]').show();
+        }
+        setTimeout(showProduct, 400);
+    });
+
+    //Muestra todos los productos
+    $(' .category_item[category="all"]').click(function () {
+        function showAll() {
+            $(".product-item").show();
+            $(".product-item").css("transform", "scale(1)");
+        }
+        setTimeout(showAll, 400);
+    });
+
+    //Filtrar por precio 
+    $(".precio_item").click(function () {
+        var catProduct = $(this).attr("price");
+        console.log(catProduct);
+        //Agregando clase active al enlace secundario
+        $(".precio_item").removeClass("ct_item-active");
+        $(this).addClass("ct_item-active");
+
+        //Ocultando Productos
+        $(".product-item").css("transform", "scale(0)");
+
+        function hideProduct() {
+            $(".product-item").hide();
+        }
+        setTimeout(hideProduct, 400);
+
+        //mostrando productos
+        function showProduct() {
+            $('.product-item[price="' + catProduct + '"]').css(
+                "transform",
+                "scale(1)"
+            );
+            $('.product-item[price="' + catProduct + '"]').show();
+        }
+        setTimeout(showProduct, 400);
+    });
+});
